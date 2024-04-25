8e8151007bc129f2d3a8ccdb80a47e79e51d4e9a
Rezoan Shakil Prince <prince.cuet.77@gmail.com>
2024-04-25 12:17:07 +0600

    Project initialization

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-red-100 text-center'>React Application</h1>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
